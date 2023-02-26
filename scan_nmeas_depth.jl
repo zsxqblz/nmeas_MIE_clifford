77bae440e1667823f0564c5b6ae157016b489ab7
zsxqblz <zsxqblz@gmail.com>
2023-02-25 21:08:28 -0500

    add slurm job array

---
+++ scan_nmeas_depth.jl
@@ -0,0 +1,22 @@
+include("dependencies.jl")
+include("sim.jl")
+include("exp.jl")
+
+const n_meas_start = parse(Int64,ARGS[1])
+const n_meas_end = parse(Int64,ARGS[2])
+const n_meas_step = parse(Int64,ARGS[3])
+const n_Asites = 10
+const n_Bsites = 100
+const n_Csites = 10
+const nsim = 100
+const depth_start = parse(Int64,ARGS[4])
+const depth_end = parse(Int64,ARGS[5])
+const depth_step = parse(Int64,ARGS[6])
+const file_name = ARGS[7]
+
+const n_meas_l = floor.(Int,collect(range(n_meas_start,stop=n_meas_end,step=n_meas_step)))
+const n_meas_length = length(n_meas_l)
+const depth_l = floor.(Int,collect(range(n_meas_start,stop=n_meas_end,step=n_meas_step)))
+const depth_length = length(n_meas_l)
+cmi_ave,cmi_std = scanNmeasDepthBW(n_Asites,n_Bsites,n_Csites,n_meas_start,n_meas_end,n_meas_step,depth_start,depth_end,depth_step,nsim,true)
+save2DData(n_meas_l,depth_l,cmi_ave,cmi_std,file_name)
---
+++ scan_nmeas_depth.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=scan_nd
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=02:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=begin        # send email when job begins
+#SBATCH --mail-type=end          # send email when job ends
+#SBATCH --array=0-99
+#SBATCH --mail-user=yz4281@princeton.edu
+
+julia scan_nmeas_depth.jl 80 100 2 60 100 2 data/230225/230225_nd1_$SLURM_ARRAY_TASK_ID
